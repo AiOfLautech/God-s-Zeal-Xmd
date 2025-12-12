c047805baf6ff235c5a7fb49bb308300cbe1d1f3
AiOfLautech <babalolahephzibah2@gmail.com>
2025-12-12 07:26:34 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- main.js
+++ main.js
@@ -146,7 +146,7 @@ const soraCommand = require('./commands/sora');
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
+global.channelLink = "https://whatsapp.com/channel/0029VaXKAEoKmCPS6Jz7sw0N";
 global.ytch = "GodsZeal";
 
 // Add this near the top of main.js with other global configurations
@@ -197,7 +197,7 @@ async function handleMessages(sock, messageUpdate, printLog) {
             
             if (buttonId === 'channel') {
                 await sock.sendMessage(chatId, { 
-                    text: '📢 *Join our Channel:*\nhttps://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A' 
+                    text: '📢 *Join our Channel:*\nhttps://whatsapp.com/channel/0029VaXKAEoKmCPS6Jz7sw0N' 
                 }, { quoted: message });
                 return;
             } else if (buttonId === 'owner') {
@@ -206,7 +206,7 @@ async function handleMessages(sock, messageUpdate, printLog) {
                 return;
             } else if (buttonId === 'support') {
                 await sock.sendMessage(chatId, { 
-                    text: `🔗 *Support*\n\nhttps://chat.whatsapp.com/GA4WrOFythU6g3BFVubYM7?mode=wwt` 
+                    text: `🔗 *Support*\n\nhttps://chat.whatsapp.com/L7lhDJmNj2s1w6lLjxaB6e` 
                 }, { quoted: message });
                 return;
             }
@@ -1260,3 +1260,4 @@ module.exports = {
     }
 
 };
+
